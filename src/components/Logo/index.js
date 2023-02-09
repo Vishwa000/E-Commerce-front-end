d19e98d8ae5fca47f24d97fc23f3abbea99f22c2
Vishwa000 <vichuvishwa004@gmail.com>
2023-02-09 07:19:18 +0530

    e-commerce front-end

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Logo = props => {
-    return (
-        <Link to="/">
-            <div className="Logo" {...props}>
-                {/* <img src="./logo/logo.png" /> */}
-                <span>Khan Store</span>
-            </div>
-        </Link>
-        
-    );
-}
+const Logo = (props) => {
+  return (
+    <Link to="/">
+      <div className="Logo" {...props}>
+        {/* <img src="./logo/logo.png" /> */}
+        <span>JRC Store</span>
+      </div>
+    </Link>
+  );
+};
 
 export default Logo;
